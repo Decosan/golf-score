0df4d702e3117896f8ba65c5af7aa1db4068c9f9
junichiro.suzuki <junx0401@gmail.com>
2019-02-23 15:00:26 +0000

    add search for golf-score

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,12 +2,18 @@ class CoursesController < ApplicationController
   before_action :require_user_logged_in
   
   def index
-    @courses=Course.all.order('alphabet ASC').page(params[:page])
+    @courses=Course.search(params[:search])
     @scores=current_user.scores.all
   end
   
   def show
     @scores=current_user.scores.where(course_id: params[:id]).order('date DESC').page(params[:page])
     @course=Course.find(params[:id])
+  end
+  
+  private
+  
+  def course_params
+    params.require(:course).permit(:name)
   end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,11 @@
 class Course < ApplicationRecord
   has_many :scores
+  
+  def self.search(search)
+    if search
+      where("name LIKE ?","%#{search}%")
+    else
+      all.order("alphabet ASC")
+    end
+  end
 end
